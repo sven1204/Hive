6c19a59d0cc8b002077c7aa09df1f3073c0e0a1d
sven-grppn <sven.grppn@eduge.ch>
2026-09-24 00:35:44 +0200

    Nouveau logo : rayon de miel (remplace l'œuf cassé), favicon et icônes d'app mis à jour

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useTheme } from "../context/ThemeContext";
 import { useSocket } from "../context/SocketContext";
-import logoHive from "../img/logoHive.png";
+import logoHive from "../img/logoHive.svg";
 import classes from "./Header.module.css";
 import { useTranslation } from "react-i18next";
 import { MessageSquare, Bell, X, Check, Menu, Sun, Moon, Plus, LogOut } from "lucide-react";
